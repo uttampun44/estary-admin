b2b09af502cafaf908b37cf933d97a731ec9388c
uttam <uttampun62@gmail.com>
2024-01-04 20:58:37 +0545

    estary agent admin

---
+++ inc/header-admin.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Real Estary Admin</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+   <link rel="stylesheet" href="../css/index.css" />
+</head>
+<body>
+    <header class="header_container">
+           <div class="container">
+                <div class="container">
+                      <div class="header_row d-flex justify-content-between align-items-center py-4">
+                           <div class="title_searchbar d-flex align-items-center">
+                              <h1 class="text-white fst-4 font-weight-bold">Estary Admin</h1>
+                              <input name="search"  placeholder="Search Here" class="py-2 rounded px-2"/>
+                           </div>
+
+                           <div class="username_image">
+                               <img src="" alt="userprofile" />
+                               <p class="text-white fst-6 font-weight-normal">Hawkins Maru</p>
+                           </div>
+                      </div>
+                </div>
+           </div>
+    </header>
+</body>
+</html>
---
+++ view/dashboard.php
@@ -0,0 +1,21 @@
+<?php include_once('../inc/header-admin.php'); ?>
+<main>
+    <section>
+          <div class="sider_bar">
+              <div class="siderbar_cols">
+                    <div class="side">
+                         <aside>
+                              <ul>
+                                   <li class="my-4"><a class="text-white text-decoration-none">Dashboard</a></li>
+                                   <li class="my-4"><a class="text-white text-decoration-none">Property</a></li>
+                                   <li class="my-4"><a class="text-white text-decoration-none">Agents</a></li>
+                                   <li class="my-4"><a class="text-white text-decoration-none">Analytics</a></li>
+                                   <li class="my-4"><a class="text-white text-decoration-none">Reviews</a></li>
+                                   <li class="my-4"><a class="text-white text-decoration-none cursor-pointer">My Profile</a></li>
+                              </ul>
+                         </aside>
+                    </div>
+              </div>
+          </div>
+    </section>
+</main>
